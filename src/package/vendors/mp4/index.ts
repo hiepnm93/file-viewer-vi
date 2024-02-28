97010f57c3c765d9fa621f789cdb5b0822a232b9
wangyu <727842003@qq.com>
2024-02-28 16:09:53 +0800

    feat: 视频居中

--- src/package/vendors/mp4/index.ts
+++ src/package/vendors/mp4/index.ts
@@ -6,6 +6,10 @@ export default function(buffer: ArrayBuffer, target: HTMLDivElement) {
   mp4.width = 840
   mp4.height = 480
   mp4.controls = true
+  Object.assign(mp4.style, {
+    margin: '40px auto',
+    display: 'block'
+  })
   const source = document.createElement('source')
   source.src = URL.createObjectURL(new Blob([buffer]))
   mp4.appendChild(source)
